984ee9036a3cdef7e6e978af91546c55516e253d
Bernat Bonda <bernat_bonda@epam.com>
2018-10-03 16:31:28 +0200

    Login method

--- src/main/java/bookmarks/authentication/AuthenticationFacade.java
+++ src/main/java/bookmarks/authentication/AuthenticationFacade.java
@@ -7,10 +7,14 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationFacade {
-    public void logout(String userIdValue, String accessTokenId) {
+    public void login(String userName, String password){
+
     }
 
-    public boolean isAuthenticated(String userIdValue, String accessTokenId) {
+    public void logout(String userId, String accessTokenId) {
+    }
+
+    public boolean isAuthenticated(String userId, String accessTokenId) {
         return true;
     }
 }
