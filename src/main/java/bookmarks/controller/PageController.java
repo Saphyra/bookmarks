304ad72432ddfee62d1e9b0ef719310957e6be91
Bernat Bonda <bondabear@gmail.com>
2018-10-04 17:42:24 +0200

    Links base

--- src/main/java/bookmarks/controller/PageController.java
+++ src/main/java/bookmarks/controller/PageController.java
@@ -11,10 +11,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PageController {
     public static final String INDEX_MAPPING = "/";
+    public static final String LINKS_MAPPING = "/links";
 
     @GetMapping(INDEX_MAPPING)
     public String index(){
         log.info("Request arrived to {}", INDEX_MAPPING);
         return "index";
+    }
+
+    @GetMapping(LINKS_MAPPING)
+    public String links(){
+        log.info("Request arrived to {}", LINKS_MAPPING);
+        return "links";
     }
 }
