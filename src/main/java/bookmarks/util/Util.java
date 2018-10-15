572bee445fe79c4855c6a5e87fa809257c12f48b
Bernat Bonda <bondabear@gmail.com>
2018-10-15 21:45:19 +0200

    Sleep in util

--- src/main/java/bookmarks/util/Util.java
+++ src/main/java/bookmarks/util/Util.java
@@ -6,4 +6,12 @@ public class Util {
     public static <T> T replaceIfNotNull(T original, T target){
         return Optional.ofNullable(target).orElse(original);
     }
+
+    public static void sleep(long delay){
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
